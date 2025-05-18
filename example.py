d5b0a61eb9b68039ba1998e7314f1e3c8cc2960d
Adarshkumar61 <adarshb2k61@gmail.com>
2025-05-18 10:16:47 +0530

    updated

--- example.py
+++ example.py
@@ -6,13 +6,14 @@ def create_cnn_model(input_shape, num_classes):
     model = models.Sequential()
 
     # Convolutional layer 1
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape)) #relu is non-linearilty
+    #for understanding better complexity undestanding 
     model.add(layers.MaxPooling2D((2, 2)))
 
     # Convolutional layer 2
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
-
+ 
     # Convolutional layer 3
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
