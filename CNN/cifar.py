908bb5ee17c45a8772a6c1ae05f88ad3fe6f24c7
Adarshkumar61 <adarshb2k61@gmail.com>
2025-05-19 10:30:14 +0530

    graph added

--- CNN/cifar.py
+++ CNN/cifar.py
@@ -6,3 +6,17 @@ from tensorflow.keras.datasets import cifar10
 #dataset loading:
 
 (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+
+#giving class names to images:
+class_name= ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
+
+#plotting:
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap= plt.cm.binary)
+    plt.xlabel(class_name[train_labels[i][0]])
+    plt.show()
