de80890a96aea9720e40e2271e8477265f4354ad
Adarshkumar61 <adarshb2k61@gmail.com>
2025-05-19 10:25:11 +0530

    Loaded the dataset

--- CNN/cifar.py
+++ CNN/cifar.py
@@ -2,3 +2,7 @@ import tensorflow as tf
 from tensorflow.keras import models, layers
 import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import cifar10
+
+#dataset loading:
+
+(train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
