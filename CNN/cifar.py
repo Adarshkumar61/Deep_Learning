0edad2a56765d4c40285f96bc0aedee32c8d3d19
Adarshkumar61 <adarshb2k61@gmail.com>
2025-06-01 11:04:50 +0530

    updated

--- CNN/cifar.py
+++ CNN/cifar.py
@@ -16,7 +16,7 @@ plt.figure(figsize=(10,10))
 for i in range(25):
     plt.subplot(5,5, i+1)
     plt.xticks([])
-    plt.yticks([])
+    plt.yticks([]) 
     plt.grid(False)
     plt.imshow(train_images[i], cmap= plt.cm.binary)
     plt.xlabel(class_name[train_labels[i][0]])
