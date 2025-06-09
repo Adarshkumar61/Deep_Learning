67f4baf66b7b2aa256794cc09e1cb73646e3cda1
Adarshkumar61 <adarshb2k61@gmail.com>
2025-06-09 10:10:04 +0530

    Updated

--- CNN/mnist.py
+++ CNN/mnist.py
@@ -9,3 +9,8 @@ from keras.datasets import datasets
 
 #loading the Dataset:
 (X_train, y_train), (X_test, y_test) = mnist.dataset()
+
+#checking the size of data:
+
+X_train.shape # 60000 ,28,28 means there are 60k images and the dimension of image is 28 * 28 
+X_test.shape # 10000, 28, 28 means there are 10k images in training and size of the image is 28* 28
