2b6a5aabcf23601e8c837ee7bf768f4dcd920e92
Adarshkumar61 <adarshb2k61@gmail.com>
2025-06-09 10:15:28 +0530

    updated

--- CNN/mnist.py
+++ CNN/mnist.py
@@ -14,3 +14,15 @@ from keras.datasets import datasets
 
 X_train.shape # 60000 ,28,28 means there are 60k images and the dimension of image is 28 * 28 
 X_test.shape # 10000, 28, 28 means there are 10k images in training and size of the image is 28* 28
+
+# displaying an image:
+plt.imshow(X_train[2])
+plt.show()
+#printing also the label:
+print(y_train[2]) #it will print the label which is 4
+
+#now we will do the scaling the values:
+# bw 0 to 1:
+X_train = X_train/255
+X_test = X_test/255
+
