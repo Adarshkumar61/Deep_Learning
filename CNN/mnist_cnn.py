bf1ae407bf7e13002d43d21637ccbea457be59a7
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-18 10:18:29 +0530

    updated

---
+++ CNN/mnist_cnn.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
+
+# laod data :
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+#reshape and resize:
+
+x_train = x_train.reshape(-1, 28,28, 1).astype('float32') / 255
+x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255
+
+# Build the model:
+model = models.sequential([
+    layers.Conv2D(32, (3,3), )
+])
