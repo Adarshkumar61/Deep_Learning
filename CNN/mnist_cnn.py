990258fdf248d9f612b4f88d7cb5cd0e4439ba68
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-18 10:31:04 +0530

    updated

--- CNN/mnist_cnn.py
+++ CNN/mnist_cnn.py
@@ -15,5 +15,10 @@ x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255
 model = models.sequential([
     layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (28, 28, 1))
     layers.MaxPooling2D(2,2),
+    layers.Conv2D(64, (3,3), activation = 'relu'),
+    layers.MaxPooling2D(2,2),
+    layers.Flatten(),
+    layers.Dense(128, activation = 'relu'),
+    layers
     
 ])
