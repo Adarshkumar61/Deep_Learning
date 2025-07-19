2f9810ead5926de3d708e9cddd76a87e5c728016
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-19 09:47:51 +0530

    updated

--- CNN/mnist_cnn.py
+++ CNN/mnist_cnn.py
@@ -13,14 +13,30 @@ x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255
 
 # Build the model:
 model = models.sequential([
-    layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (28, 28, 1))
+    layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (28, 28, 1)),
     layers.MaxPooling2D(2,2),
     layers.Conv2D(64, (3,3), activation = 'relu'),
     layers.MaxPooling2D(2,2),
     layers.Flatten(),
     layers.Dense(128, activation = 'relu'),
     layers.Dense(10, activation = 'softmax')
-    
 ])
 
 #compile the model:
+
+model.compile(
+    optimizer = 'adam',
+    loss = 'sparse_categorical_crossentropy',
+    metrics = ['accuracy']
+)
+
+
+# training :
+history = model.fit(x_train, y_train, ephocs = 5, valdation_data = (x_test, y_test))
+
+
+# evaluation:
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+
+print(f'Test accuracy is: {test_acc: .2f}')
