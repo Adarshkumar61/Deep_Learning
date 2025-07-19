0722d73cdc4cb5f6da40c3b05ea48c6dee3000ca
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-19 09:49:58 +0530

    updated

--- CNN/mnist_cnn.py
+++ CNN/mnist_cnn.py
@@ -40,3 +40,4 @@ history = model.fit(x_train, y_train, ephocs = 5, valdation_data = (x_test, y_te
 test_loss, test_acc = model.evaluate(x_test, y_test)
 
 print(f'Test accuracy is: {test_acc: .2f}')
+test accuracy
