3535226cedbdb06dc073fdb5571dd80504e2e96c
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-23 20:12:21 +0530

    updated

--- CNN/model_using_data_augmentation_with_cifar10_dataset.py
+++ CNN/model_using_data_augmentation_with_cifar10_dataset.py
@@ -9,3 +9,10 @@ import matplotlib.pyplot as plt
 # data loading:
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
+x_train =  x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
