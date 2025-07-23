448f2530fd549761f29da3b3d1f5d97cab6524bc
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-23 19:17:40 +0530

    Updated

--- CNN/model_with_cifar10_with_Batch_Norm_and_dropout.py
+++ CNN/model_with_cifar10_with_Batch_Norm_and_dropout.py
@@ -16,8 +16,6 @@ y_test = tf.keras.utils.to_categorical(y_test, 10)
 model = models.sequential()
 
 #Block 1:
-padding = 2
-activation = 3
 model.add(layers.Conv2D(32, (3,3), padding = 'same', activation = 'relu', input_shape = (28, 28, 3)))
 model.add(layers.BatchNormalization())
 model.add(layers.Conv2D(32, (3,3), padding = 'same', activation = 'relu'))
@@ -52,3 +50,24 @@ model.add(layers.Dropout(0.25))
 
 
 #compiling the Model:
+
+model.compile(
+    optimizer = 'adam',
+    loss = 'categorical_crossentropy',
+    metrics = ['accuracy']
+)
+
+model.fit(x_train, y_train, ephocs = 10, validation_data = (x_test, y_test))
+
+loss_test, acc_test = model.evaluate(x_test, y_test)
+
+print(f'accuracy of the model is: {acc_test}')
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+img_index = 4
+img = x_test[img_index]
+
+actual = y_test[img_index]
+
+prediction = model.predict(np.expand_dims(img, axis= 0))
+predicted = np.argmax(prediction)
