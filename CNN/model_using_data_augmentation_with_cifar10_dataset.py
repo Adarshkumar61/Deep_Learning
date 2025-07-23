360fe75b681d97313ac7d12d51d1b96a86666172
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-23 20:08:44 +0530

    updated

---
+++ CNN/model_using_data_augmentation_with_cifar10_dataset.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tf.keras.datasets import cifar10
+from tf.keras import layers, models
+from tf.keras.utils import to_categorical
+from tf.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import matplotlib.pyplot as plt
+
