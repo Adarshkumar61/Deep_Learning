2a82cdf61cb86d4b5a51704cfb39fb989ed10f19
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-25 09:20:27 +0530

    updated

--- CNN/model_using_data_augmentation_with_cifar10_dataset.py
+++ CNN/model_using_data_augmentation_with_cifar10_dataset.py
@@ -35,9 +35,11 @@ model.add(layers.BatchNormalization())
 model.add(layers.MaxPooling2D(2,2))
 model.add(layers.Dropout(0.2))
 
-# BLOCK3:
+
 # BLOCK3:
 model.add(layers.Conv2D(128, padding = 'same', activation = 'relu'))
 model.add(layers.BarchNormalization())
 model.add(layers.Conv2D(128, padding = 'same', activation = 'relu'))
-model.add
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Dropout(0.2))
