4c76af11cc386cb8611c29159d8e8c11de7638e7
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-01 11:03:14 +0530

    updated

--- CNN/Transfer_Learning_using_RestNet.py
+++ CNN/Transfer_Learning_using_RestNet.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from 
-import numpy as np
-import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input
+import matplotlib.pyplot as plt 
 
