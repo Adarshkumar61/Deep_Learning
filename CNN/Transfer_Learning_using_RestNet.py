7f328bbc13712b23c9b74b2b167fed745d87c90f
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-04 19:23:52 +0530

    Code Updated

--- CNN/Transfer_Learning_using_RestNet.py
+++ CNN/Transfer_Learning_using_RestNet.py
@@ -6,6 +6,7 @@ from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+import os
 import numpy as np
 import matplotlib.pyplot as plt 
 
@@ -53,16 +54,46 @@ for layer in res_net.layers:
     x = Dropout(0.3)(x)
     x = Dense(128, activation = 'relu')(x)
     x = Dropout(0.2)(x)
-    prediction = Dense(train_gen.num_classes, activation = 'softmax')(x)
+    output = Dense(train_gen.num_classes, activation = 'softmax')(x)
     
-    model = Model(res_net.input, output = prediction)
+    model = Model(res_net.input, output = output)
     
     # now we gonna compile the model:
     model.compile(
-        optimizer = Adam(), 
+        optimizer = Adam(Learning_rate = 0.001), 
         loss = 'categorical_crossentropy',
         metrics = ['accuracy']
     )
     
     # we will see the summary :
     model.summary()
+    
+    history = model.fit(train_gen, epochs = 10, validation_data = val_gen)
+    
+    
+    #prediction:
+    
+    img_path = ''
+    img  = image.load_img(img_path, target_size = (img_size, img_size))
+    img_array = image.to_array(img)
+    
+    expand_dim_img = np.expand_dims(img_array, 0)
+    expand_dim_img = preprocess_input(expand_dim_img)
+    
+    prediction = model.predict(expand_dim_img)
+    predicttion_final = np.argmax(prediction[0])
+    
+    class_label = list(train_gen.class_indices.keys())
+    print(f'predicted class: {class_label[predicttion_final]}')
+    
+    #visulaize:
+    img_display = image.load_image(img_path, target_size = (img_size, img_size))
+    plt.imshow(img_display)
+    plt.axis('off')
+    plt.title(f'predicted: {predicttion_final}, actual: {os.path.basename(os.path.dirname(img_path))}') 
+    
+    
+    # plt.plot(history.history['accuracy'], label='Train Accuracy')
+    # plt.plot(history.history['val_accuracy'], label='Val Accuracy')
+    # plt.legend()
+    plt.show()
