4d53cfffed17e10d2899041b7ef2689be8d010d6
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-04 19:27:54 +0530

    Imported Required Libraries

---
+++ CNN/Transfer_Learning_Using_VGG.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import zipfile
+import os
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.optimizers import Adam
