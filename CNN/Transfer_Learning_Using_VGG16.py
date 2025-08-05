796c1446d63f5c14e47097b8b29222da67decc5f
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-05 13:42:58 +0530

    updated

--- CNN/Transfer_Learning_Using_VGG16.py
+++ CNN/Transfer_Learning_Using_VGG16.py
@@ -27,3 +27,63 @@ datagen = ImageDataGenerator(
 )
 
 #data split into training and validation:
+
+train_gen = datagen.flow_from_directory(
+    '/contentt/flowers',
+    target_size = (img_size, img_size),
+    batch_size = batch_size,
+    subset = 'training',
+    class_mode = 'categorical'
+)
+
+val_gen = datagen.flow_from_directory(
+    '/contentt/flowers',
+    target_size = (img_size, img_size),
+    batch_size = batch_size,
+    subset = 'testing',
+    class_mode = 'categorical'
+)
+
+vgg = VGG16(include_top = False, weights = 'imagenet', input_shape = (img_size, img_size, 3))
+
+
+#frezzing training:
+for layer in vgg.layers:
+    layer.trainable = False
+#adding top layer:
+x = Flatten()(vgg.output)
+x = Dense(128, activation = 'relu')(x)
+x = Dropout(0.2)(x)
+output = Dense(train_gen.num_classes, activation = 'softmax')(x)
+
+
+# calling the model:
+model = Model(vgg.input, output = output)
+
+
+# compling the model;
+model.compile(
+    optimizer = Adam(),
+    loss = 'categorical_crossentropy',
+    metrics = ['accuracy']
+)
+
+
+history = model.fit(train_gen, epochs = epochs, validation_data = val_gen)
+
+# prediction:
+
+img_path = ' '
+img = image.load_img(img_path, target_size = (img_size, img_size))
+img_in_arr = image.to_array(img)
+
+exp_dim = np.expand_dims(img_in_arr, 0)
+exp_dim = preprocess_input(exp_dim)
+
+prediction = model.predict(exp_dim)
+prediction_final = np.argmax(prediction[0])
+
+class_label = list(train_gen.num_indices.keys())
+print(f'Prediciton is: {class_label[prediction_final]}')
+
+print(f'actual label is: {class_label[prediction_final]}')
