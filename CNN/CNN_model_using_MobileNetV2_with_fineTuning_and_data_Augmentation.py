ded5f2027b6ec4603a124d63360aa93f8bf51fa1
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-06 11:12:58 +0530

    updated

--- CNN/CNN_model_using_MobileNetV2_with_fineTuning_and_data_Augmentation.py
+++ CNN/CNN_model_using_MobileNetV2_with_fineTuning_and_data_Augmentation.py
@@ -8,3 +8,37 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing import preprocess_input
 from tensorflow.keras.preprocessing import image
 
+
+# Paths to your data
+path = ' '
+
+# Data augmentation for training
+datagen = ImageDataGenerator(
+    rescale=1./255,
+    preprocessing_function = preprocess_input,
+    validation_split = 0.2,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+# Data generators
+train_generator = datagen.flow_from_directory(
+    path,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+val_generator = datagen.flow_from_directory(
+    path,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+# Load MobileNetV2 base model
