6eddd303a6d5f23b777c14700fda791ef6fbc7bb
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-06 11:12:11 +0530

    updated

--- CNN/CNN_model_using_MobileNetV2_with_fineTuning_and_data_Augmentation.py
+++ CNN/CNN_model_using_MobileNetV2_with_fineTuning_and_data_Augmentation.py
@@ -1,1 +1,10 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing import preprocess_input
+from tensorflow.keras.preprocessing import image
 
