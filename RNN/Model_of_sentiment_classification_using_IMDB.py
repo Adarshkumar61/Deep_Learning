6ba68180746543e2ba825709770ef2ea01d471cd
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-11 19:49:27 +0530

    updated

---
+++ RNN/Model_of_sentiment_classification_using_IMDB.py
@@ -0,0 +1,6 @@
+# importing required libraries:
+import tensorflow as tf
+from tensorflow.keras.models import sequential
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing.sequence import pad_sequence
+from tensorflow.keras.
