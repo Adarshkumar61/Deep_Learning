c4b1887f4b890cdcc6b1eeef3c7d96193d835541
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-11 19:54:24 +0530

    updated

--- RNN/Model_of_sentiment_classification_using_IMDB.py
+++ RNN/Model_of_sentiment_classification_using_IMDB.py
@@ -3,4 +3,6 @@ import tensorflow as tf
 from tensorflow.keras.models import sequential
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing.sequence import pad_sequence
-from tensorflow.keras. 
+from tensorflow.keras.layers import Dense, Embedding, SimpleRNN
+
+#laod and preprocess the data:
