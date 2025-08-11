ec65be6222fdbb33b08c7f7a2756ec5bf01cce7b
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-11 19:55:35 +0530

    updated

--- RNN/Model_of_sentiment_classification_using_IMDB.py
+++ RNN/Model_of_sentiment_classification_using_IMDB.py
@@ -6,3 +6,5 @@ from tensorflow.keras.preprocessing.sequence import pad_sequence
 from tensorflow.keras.layers import Dense, Embedding, SimpleRNN
 
 #laod and preprocess the data:
+num_words = 10000 # means loading data with 10k frequent words
+(x_train, )
