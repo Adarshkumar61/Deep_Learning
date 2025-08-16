ab0d3110f3f65a5d8e92b7d83564e3712bf89ba5
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-16 11:01:13 +0530

    fitted model

--- LSTM/basic_sequential_lstm_model.py
+++ LSTM/basic_sequential_lstm_model.py
@@ -1,6 +1,30 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 
 seq_len = 3
 
+data = np.array([i for i in range(50)])
+
+x, y = [], []
+for i in range(len(data)- seq_len):
+    x.appned(data[i:i+seq_len])
+    y.append(data[i+seq_len])
+
+
+    x= np.array(x)
+    y = np.array(y)
+
+    x = x.reshape(x.shape[0], x.shape[1], 1)
+
+    model = Sequential()
+    model.add(LSTM(50, activaiton = 'relu', input_shape= (seq_len, 1)))
+    model.add(Dense(1))
+
+    model.compile(
+        optimizer = 'adam',
+        loss = 'mse'
+    )
+
+    model.fit(x, y, epochs = 200, verbose = 1)
