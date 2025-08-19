209e17133ad65ba98e15c699dab57c558fb0e406
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-19 18:38:54 +0530

    model created with prediciton

--- LSTM/word_based_LSTM_Model_with_own_data_set.py
+++ LSTM/word_based_LSTM_Model_with_own_data_set.py
@@ -51,3 +51,16 @@ model.add(Dense(total_words, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(x, y, epochs = 500, verbose = 0)
 
+# Function to generate text
+def generate_text(seed_text, next_words=5):
+    for _ in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list], maxlen=max_len-1, padding='pre')
+        predicted = model.predict(token_list, verbose=0)
+        predicted_word_index = np.argmax(predicted, axis=-1)[0]
+        output_word = tokenizer.index_word[predicted_word_index]
+        seed_text += " " + output_word
+    return seed_text
+# Example usage
+
+
