68efa4561d994247238bbf16e78198f8dc472f47
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-19 10:02:30 +0530

    updated

--- LSTM/word_based_LSTM_Model_with_own_data_set.py
+++ LSTM/word_based_LSTM_Model_with_own_data_set.py
@@ -6,3 +6,18 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow .keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical
 
+data = [
+    "I love programming in Python",
+    "Deep learning is fascinating",
+    "LSTM networks are powerful for sequence prediction",
+    "Natural language processing is a key area in AI",
+    "TensorFlow makes building models easier",
+    "Keras is a high-level API for TensorFlow",
+    "Neural networks can learn complex patterns",
+    "Data science combines statistics and programming",
+    "Machine learning is a subset of AI",
+    "Artificial intelligence is transforming industries",
+    "I love robots"
+]
+
+
