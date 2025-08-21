d1bc9a2ff1d44b1b07c522f1572f306bddb3102b
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-21 09:48:06 +0530

    updated

--- LSTM/stacked_LSTM_model_using_random_num_dataset.py
+++ LSTM/stacked_LSTM_model_using_random_num_dataset.py
@@ -3,3 +3,15 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 import numpy as np
 
+x =np.random.rand(1000, 10, 1)
+y = np.random.rand(1000, 1)
+
+model = Sequential([
+    LSTM(50, return_sequences= True, input_shape=(10, 1)),
+    Dropout(0.2),
+    LSTM(50, return_sequences = False),
+    Dropout(0.2),
+    Dense(1)
+])
+
+
