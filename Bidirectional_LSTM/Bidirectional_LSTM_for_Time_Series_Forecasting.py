2e5f8c6373491d55afca8a463ef467884ddab877
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-22 09:47:28 +0530

    updated

--- Bidirectional_LSTM/Bidirectional_LSTM_for_Time_Series_Forecasting.py
+++ Bidirectional_LSTM/Bidirectional_LSTM_for_Time_Series_Forecasting.py
@@ -9,3 +9,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 df = pd.read_csv('Apple.csv')
 df.isnull().sum()
 
+price = df['close'].values.reshape(-1, 1)
+scaler = MinMaxScaler(feature_range= (0,1))
+scaled_data = scaler.fit_transform(price)
+
