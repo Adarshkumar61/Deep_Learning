fd27835038ec329891447c305671ebb6bd77bf69
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-29 09:58:17 +0530

    updated

--- LSTM/Netflix_Stock_Price_Prediction.py
+++ LSTM/Netflix_Stock_Price_Prediction.py
@@ -10,3 +10,12 @@ d2 = date.today() - timedelta(days=5000)
 d2 = d2.strftime("%Y-%m-%d")
 start_date = d2
 
+data = yf.download('NFLX', 
+                      start=start_date, 
+                      end=end_date, 
+                      progress=False)
+data["Date"] = data.index
+data = data[["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]]
+data.reset_index(drop=True, inplace=True)
+print(data.tail())
+
