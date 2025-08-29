d826766617910f2929661c8425887b8db5e8953c
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-29 09:57:49 +0530

    updated

--- LSTM/Netflix_Stock_Price_Prediction.py
+++ LSTM/Netflix_Stock_Price_Prediction.py
@@ -3,3 +3,10 @@ import yfinance as yf # to download stock data ("NFlX")
 import datetime
 from datetime import date, timedelta
 today = date.today()
+
+d1 = today.strftime("%Y-%m-%d")
+end_date = d1
+d2 = date.today() - timedelta(days=5000)
+d2 = d2.strftime("%Y-%m-%d")
+start_date = d2
+
