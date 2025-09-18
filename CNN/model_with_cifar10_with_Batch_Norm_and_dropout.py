25f7cefc62035f77ec8ef70c36883377c566edd3
Adarshkumar61 <adarshb2k61@gmail.com>
2025-09-18 09:11:04 +0530

    updated

--- CNN/model_with_cifar10_with_Batch_Norm_and_dropout.py
+++ CNN/model_with_cifar10_with_Batch_Norm_and_dropout.py
@@ -7,8 +7,8 @@ import numpy as np
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 
-x_train = x_train / 255
-x_test = x_test / 255
+x_train = x_train / 255.0 
+x_test = x_test / 255.0
 
 y_train = tf.keras.utils.to_categorical(y_train, 10)
 y_test = tf.keras.utils.to_categorical(y_test, 10)
